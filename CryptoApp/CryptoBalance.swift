673bf38df74ee46bed5b9fb71c2c21dfa929aef4
Iosi Pratama <pratamaiosi@gmail.com>
2023-12-01 09:23:21 +0800

    Add data model and data service

---
+++ CryptoApp/CryptoBalance.swift
@@ -0,0 +1,8 @@
+//
+//  CryptoBalance.swift
+//  CryptoApp
+//
+//  Created by Iosi Pratama on 01/12/23.
+//
+
+import Foundation
---
+++ CryptoApp/DataService.swift
@@ -0,0 +1,8 @@
+//
+//  DataService.swift
+//  CryptoApp
+//
+//  Created by Iosi Pratama on 01/12/23.
+//
+
+import Foundation
